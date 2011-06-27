f4a5849f74fb483f83922e357f81e366b5282248
James Dennis <jd@j2labs.net>
2011-06-27 12:50:27 -0400

    Oh man this environment rules now

--- etc/arbre_env.sh
+++ etc/arbre_env.sh
@@ -1,29 +1,23 @@
 # Arbre Environment
-
+#
 # This script sets a few environment variables and defines some functions 
-# intended to make developing with Arbre easy.
+# intended to make development with Arbre easy.
 #
-# It's design is inspired by virtualenvwrapper [1]
+# The top of this file must be functions. The functions that affect your shell
+# environment are called after the function definitions shell commands are
+# escaped with a '\' to avoid accidental use of shell aliases.
 #
-# `cdapp` with no arguments prints a list of apps in the arbreapp directory.
-# With arguments it cd's the user into the app's source directory.
-#
-# Shell commands are escaped with a '\' to avoid accidental use of shell aliases
-# 
-# 1. http://pypi.python.org/pypi/virtualenvwrapper
+# It's design is inspired by virtualenvwrapper:
+# * http://pypi.python.org/pypi/virtualenvwrapper
+# * http://j2labs.tumblr.com/post/5181438807/quick-dirty-virtualenv-virtualenvwrapper
 
 
-### Get full path to ARBRE_DIR regardless of where this file is sourced from
-ARBRE_ENV_PATH=$(dirname ${BASH_SOURCE[@]}) # path to this file
-ARBRE_ETC_PATH=$(cd $ARBRE_ENV_PATH; pwd)
-export ARBRE_DIR=$(dirname $ARBRE_ETC_PATH)
-
-### Hardcode paths to the apps
-export PREV_PYTHONPATH=$PYTHON_PATH
-export PYTHONPATH="$ARBRE_DIR:$ARBRE_DIR/arbreapp/hltlib/:$ARBRE_DIR/arbreapp/mailman/:$ARBRE_DIR/arbreapp/rolodex/:$PYTHONPATH"
+###
+### Functions
+###
 
 ### Shortcut for cd'ing to project dir
-arbrecd () {
+arbre () {
     \cd $ARBRE_DIR
 }
 
@@ -41,7 +35,44 @@ arbreapp () {
     fi
 }    
 
+### Updates PYTHONPATH to include any directories in $ARBRE_APP_PATH
+arbresetapppaths() {
+    PYTHONPATH=$PREV_PYTHONPATH
+    for app in `\ls -l "$ARBRE_APP_PATH" | \grep "^d" | \cut -d " " -f 12`:
+    do
+        PYTHONPATH="$ARBRE_APP_PATH/$app:$PYTHONPATH"
+    done
+    export PYTHONPATH
+}
+
+### Initializes the environment for Arbre
+arbreactivate() {
+    ### Figure out the path to Arbre based on where this file `arbre_env.sh` lives
+    export ARBRE_ENV_PATH=$(dirname $ARBRE_ENV_SCRIPT)
+
+    ### Set project directory path
+    export ARBRE_DIR=$(dirname $ARBRE_ENV_PATH)
+
+    ### Set var for app path
+    export ARBRE_APP_PATH=$(cd "$ARBRE_DIR/arbreapp"; pwd)
+
+    ### Hardcode paths to the apps
+    export PREV_PYTHONPATH=$PYTHON_PATH # Modify this value and pay with yer liiife!
+
+    ### Update PYTHONPATH with a call to arbresetapppaths
+    arbresetapppaths
+}
+
 ### Returns PYTHONPATH to normal value
 arbredeactivate () {
     export PYTHONPATH=$PREV_PYTHONPATH
 }
+
+
+###
+### Environment Adjustments
+###
+
+### `source`ing this file causes anything below here to occur
+export ARBRE_ENV_SCRIPT=${BASH_SOURCE[@]}
+arbreactivate
